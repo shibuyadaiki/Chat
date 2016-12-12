fddfaa9eedaa6ff1c627104462da029493628926
Daiki Shibuya <aaaaaaaaaadddddddddd30@gmail.com>
2016-12-12 15:27:14 +0900

    first commit

---
+++ WC101.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>Chat - Login</title>
+
+  </head>
+  <body>
+    <form action="WC201.php">
+  	<h1>Chat</h1>
+    YourName  
+    <input type="text" name="inputValue" value="">
+    <input type="submit" name="name" value="Login">
+	<input type="hidden" name="mode" value="login">
+  </form>
+  </body>
+</html>
+
